647202821f244e189d9e01837713005bc06092c7
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-03-04 10:51:22 +0300

    create server, add dependencies, create first graphQL schema

---
+++ server/server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('../schema/schema');
+
+const app = express();
+const PORT = 3005;
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log(`Server started at ${PORT} port.`);
+});
