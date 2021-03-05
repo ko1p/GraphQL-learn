25a64576ca1c77d12b1bf6b5b56b69314339ab19
Anton Belko <30528165+ko1p@users.noreply.github.com>
2021-03-05 11:17:27 +0300

    create frontend for new tests

---
+++ application/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Tabs from './components/Tabs/Tabs';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './components/theme';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3005/graphql',
+
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <MuiThemeProvider theme={theme}>
+        <Tabs />
+      </MuiThemeProvider>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
 const PORT = 3005;
@@ -12,6 +13,8 @@ mongoose.connect('mongodb://localhost:27017/mydb', {
   useFindAndModify: false,
   useUnifiedTopology: true
 })
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
